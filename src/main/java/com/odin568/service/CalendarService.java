c04efbb5c2c4f470194f4e97e2590188ea45211d
Markus Zellner <markus@zellner-mail.de>
2024-08-02 19:47:29 +0200

    Upgrade Java 21

--- src/main/java/com/odin568/service/CalendarService.java
+++ src/main/java/com/odin568/service/CalendarService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -101,7 +102,7 @@ public class CalendarService implements HealthIndicator
             try {
                 LOG.debug("Started updating calendar ics");
                 StringBuilder buffer = new StringBuilder();
-                URL url = new URL(icsUrl);
+                URL url = new URI(icsUrl).toURL();
                 try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
                     String temp;
                     while ((temp = reader.readLine()) != null) {
